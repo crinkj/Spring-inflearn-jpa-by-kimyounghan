513bd03f90da2b76ad519b27fabec7a6ac94ea9c
crinkj <jcrinkey89120@gmail.com>
2022-03-04 22:54:13 +0900

    jpa study project setting

---
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -0,0 +1,15 @@
+package jpabook.jpashop;
+
+import org.aspectj.weaver.ast.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+}
