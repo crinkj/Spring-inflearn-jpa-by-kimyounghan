874918e49953a4de53e6dac453e99dddcf229e4f
John Crinkey <32498739+crinkj@users.noreply.github.com>
2022-04-06 10:48:50 +0900

    add:queryDSL 조인 및 aggregate 테스트 코드 추가

--- src/main/java/jpabook/jpashop/queryDSL/entity/QueryDSLTeamEntity.java
+++ src/main/java/jpabook/jpashop/queryDSL/entity/QueryDSLTeamEntity.java
@@ -12,19 +12,21 @@ import java.util.List;
 // JPA 는 기본생성자가 필요
 // JPA 기본스펙에서는 protected-level 까지 기본생성자를 지원해준다
 @Entity
-@Getter @Setter
-@NoArgsConstructor(access =  AccessLevel.PROTECTED)
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "QTeam")
 public class QueryDSLTeamEntity {
 
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue
     private Long id;
     private String name;
 
-    @OneToMany(mappedBy= "team")
+    @OneToMany(mappedBy = "team")
     private List<QueryDSLMemberEntity> members = new ArrayList<>();
 
-    public QueryDSLTeamEntity(String name){
+    public QueryDSLTeamEntity(String name) {
         this.name = name;
     }
 }
--- src/test/java/jpabook/jpashop/queryDSL/service/QueryDSLMemberServiceTest.java
+++ src/test/java/jpabook/jpashop/queryDSL/service/QueryDSLMemberServiceTest.java
@@ -1,9 +1,11 @@
 package jpabook.jpashop.queryDSL.service;
 
 import com.querydsl.core.QueryResults;
+import com.querydsl.core.Tuple;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jpabook.jpashop.queryDSL.entity.QQueryDSLMemberEntity;
+import jpabook.jpashop.queryDSL.entity.QQueryDSLTeamEntity;
 import jpabook.jpashop.queryDSL.entity.QueryDSLMemberEntity;
 import jpabook.jpashop.queryDSL.entity.QueryDSLTeamEntity;
 import jpabook.jpashop.springbasic1.domain.Member;
@@ -23,6 +25,7 @@ import javax.persistence.EntityManager;
 import java.util.List;
 
 import static jpabook.jpashop.queryDSL.entity.QQueryDSLMemberEntity.queryDSLMemberEntity;
+import static jpabook.jpashop.queryDSL.entity.QQueryDSLTeamEntity.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -174,7 +177,7 @@ public class QueryDSLMemberServiceTest {
     }
 
     @Test
-    public void paging1(){
+    public void paging1() {
         List<QueryDSLMemberEntity> result = queryFactory
                 .selectFrom(queryDSLMemberEntity)
                 .orderBy(queryDSLMemberEntity.username.desc())
@@ -182,5 +185,108 @@ public class QueryDSLMemberServiceTest {
                 .limit(2)
                 .fetch();
         System.out.println(result);
+    }
+
+
+    /**
+     * 집합 함수
+     */
+    @Test
+    public void aggregation() {
+        List<Tuple> result = queryFactory
+                .select(
+                        queryDSLMemberEntity.count(),
+                        queryDSLMemberEntity.age.sum(),
+                        queryDSLMemberEntity.age.avg(),
+                        queryDSLMemberEntity.age.max(),
+                        queryDSLMemberEntity.age.min()
+                )
+                .from(queryDSLMemberEntity)
+                .fetch();
+        Tuple tuple = result.get(0);
+
+        assertThat(tuple.get(queryDSLMemberEntity.count())).isEqualTo(4);
+        assertThat(tuple.get(queryDSLMemberEntity.age.sum())).isEqualTo(100);
+        assertThat(tuple.get(queryDSLMemberEntity.age.avg())).isEqualTo(25);
+        assertThat(tuple.get(queryDSLMemberEntity.age.max())).isEqualTo(40);
+        assertThat(tuple.get(queryDSLMemberEntity.age.min())).isEqualTo(10);
+    }
+
+    /**
+     * 팀의 이름과 각팀의 평균 연령을 구해라
+     */
+    @Test
+    public void getTeamAverageAge() {
+        List<Tuple> result = queryFactory
+                .select(queryDSLTeamEntity.name,
+                        queryDSLMemberEntity.age.avg())
+                .from(queryDSLMemberEntity)
+                .innerJoin(queryDSLMemberEntity.team, queryDSLTeamEntity)
+                .groupBy(queryDSLTeamEntity.name)
+                .fetch();
+        Tuple teamA = result.get(0);
+        Tuple teamB = result.get(1);
+
+        assertThat(teamA.get(queryDSLTeamEntity.name)).isEqualTo("teamA");
+        assertThat(teamA.get(queryDSLMemberEntity.age.avg())).isEqualTo(30 / 2);
+
+        assertThat(teamB.get(queryDSLTeamEntity.name)).isEqualTo("teamB");
+        assertThat(teamB.get(queryDSLMemberEntity.age.avg())).isEqualTo(70 / 2);
+    }
+
+    /**
+     * 팀 A에 소속한 모든 회원
+     */
+    @Test
+    public void join() {
+        List<QueryDSLMemberEntity> result = queryFactory
+                .selectFrom(queryDSLMemberEntity)
+                .join(queryDSLMemberEntity.team, queryDSLTeamEntity)
+                .where(queryDSLTeamEntity.name.eq("teamA")).fetch();
+
+        // 해당 컬럼값 추출후 containsExactly안에있는 단어들 포함되있는지 확인하는 방법
+        assertThat(result)
+                .extracting("username")
+                .containsExactly("member1", "member2");
+    }
+
+    /**
+     * 세타 조인 ( 연관관계 없는 필드로 내부 조인)
+     * 외부(outer) 조인 불가능 -> 다음 강의에서 on을 사용해 외부(outer) 조인 가능
+     * 예) 회원의 이름이 팀 이름과 같은 회원 조회
+     */
+    @Test
+    public void theta_join() {
+        em.persist(new QueryDSLMemberEntity("teamA"));
+        em.persist(new QueryDSLMemberEntity("teamB"));
+
+        List<QueryDSLMemberEntity> result = queryFactory
+                .select(queryDSLMemberEntity)
+                .from(queryDSLMemberEntity, queryDSLTeamEntity)
+                .where(queryDSLMemberEntity.username.eq(queryDSLTeamEntity.name))
+                .fetch();
+
+            assertThat(result)
+                    .extracting("username")
+                    .containsExactly("teamA","teamB");
+    }
+
+    /**
+     * 연관 관계 없는 필드로 외부조인 가능하게 하는 방법
+     * 예) 회원과 팀을 조인 하면서, 팀 이름이 teamA인 팀만 조인, 회원은 모두 조회
+     * JPQL: select m, t from qMember m left join m.team t on t.name = 'teamA'
+     *
+     */
+    @Test
+    public void join_on_filtering(){
+        List<Tuple> result = queryFactory
+                .select(queryDSLMemberEntity,queryDSLTeamEntity)
+                .from(queryDSLMemberEntity)
+                .join(queryDSLMemberEntity.team, queryDSLTeamEntity)
+                .on(queryDSLTeamEntity.name.eq("teamA"))
+                .fetch();
+        for(Tuple member:result){
+            System.out.println(member);
+        }
     }
 }
